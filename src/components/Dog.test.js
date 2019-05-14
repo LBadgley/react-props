eaea7998366c3b859aa8de8674d744478fd27217
Laura Badgley <l.badgleyy@gmail.com>
2019-05-13 17:23:00 -0700

    dog and header tests passing

--- src/components/Dog.test.js
+++ src/components/Dog.test.js
@@ -4,7 +4,7 @@ import Dog from './Dog';
 
 describe('Dog component', () => {
   it('renders a doggo', () => {
-    const wrapper = shallow(<Dog />);
+    const wrapper = shallow(<Dog name="Ruby" age="2" weight="7.5 lbs"/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -4,7 +4,7 @@ import Dog from './Dog';
 
 describe('header component', () => {
   it('renders a header with a doggo in it', () => {
-    const wrapper = shallow(<Dog />);
+    const wrapper = shallow(<Dog name="Ruby" age="2" weight="7.5 lbs"/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
